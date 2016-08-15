af623c34ecd62ffc79acb36f97d9e7dd4af1f957
kuswandanu <kuswandanu@gmail.com>
2016-08-16 06:18:54 +0700

    upload.php dari up.php dengan perubahan bisa membuat directory

--- upload.php
+++ upload.php
@@ -1,44 +1,66 @@
 <?php
-$target_dir = "upload/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
+if(isset($_FILES['file'])){
+    $errors= array();
+    $file_name = $_FILES['file']['name'];
+    $file_size = $_FILES['file']['size'];
+    $file_tmp = $_FILES['file']['tmp_name'];
+    $target_dir = trim($_POST['directory']);
+    $target_file = $target_dir . ($target_dir == "" ? "" : "/") . basename($file_name);
+    
+    if($file_size > 52428800) {
+        $errors[]='Sorry, your file is too large.';
     }
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-&& $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    $uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-    } else {
-        echo "Sorry, there was an error uploading your file.";
+    
+    if($target_dir != "" && !is_dir($target_dir)){
+        if(!mkdir($target_dir)){
+            $errors[]='Directory does not exist. Failed to Created Directory.';
+        }
+    }
+    
+    if(empty($errors)==true) {
+        if(move_uploaded_file($file_tmp, $target_file)){
+            echo "Success";
+        }else{
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }else{
+        print_r($errors);
     }
 }
 ?>
+  <html>
+
+  <body>
+
+    <?php if(isset($_FILES['file']) && empty($errors)==true){ ?>
+      <ul>
+        <li>Sent file:
+          <?php echo $_FILES['file']['name'];  ?>
+        </li>
+        <li>File size:
+          <?php echo $_FILES['file']['size'];  ?>
+        </li>
+        <li>File type:
+          <?php echo $_FILES['file']['type']; ?>
+        </li>
+        <?php if(isset($_POST['directory'])){ ?>
+          <li>Directory:
+            <?php echo $_POST['directory']; ?>
+          </li>
+          <?php } ?>
+      </ul>
+      <?php } ?>
+
+        <form action="" method="POST" enctype="multipart/form-data">
+          <input type="file" name="file" /> Directory
+          <input type="text" name="directory" />
+          <input type="submit" />
+
+        </form>
+        1. Upload di root : Pilih file tapi kosongkan Directory
+        <br/> 2. Upload di direktori : Pilih file dan isi Directory
+        <br/> 3. Buat direktori : Jangan memilih file kemudian isi Directory
+        <br/> // Semua file yang diupload akan me-rewrite semua file yang sudah ada
+  </body>
+
+  </html>
